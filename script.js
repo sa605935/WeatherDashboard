41648ffce33855043f50c840c295c222536c8279
Shannon Alexander <shannonmariealexander94@gmail.com>
2021-02-03 22:48:32 -0500

    added current time to script and function for rendering the city

---
+++ script.js
@@ -0,0 +1,28 @@
+var citiesArray = [];
+var cityName = (localStorage.getItem('city'));
+var cityLat = '29.76';
+var cityLon = '-95.36';
+var currentHour = moment().hours();
+var windowWidth = this.innerWidth;
+
+$('#current-date').text(moment().format('dddd, MMMM Do YYYY'));
+
+getCurrentWeather();
+
+function renderCityButtons () {
+    $('.city-buttons').empty();
+    var dataOptions = '';
+    citiesArray.forEach(function(city) {
+        var btnDiv = $('<div>');
+        var cityBtn = $('<button>');
+        cityBtn.addClass('cityBtn');
+        cityBtn.attr('data-state', city);
+        cityBtn.text(city);
+        btnDiv.append(cityBtn);
+        $('.city-buttons').prepend(btnDiv);
+        if (windowWidth <= 991) {
+            dataOptions += `<option value= '${city}'>`;
+        }
+    })
+    $('#cities').html(dataOptions);
+}
